aa5d30eaa3a4feef402959fa914113bde5fb37f2
oeleomar <eleomar.dev@gmail.com>
2025-02-25 12:11:35 -0400

    add new features

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,16 +1,11 @@
 export default ({ env }) => ({
   upload: {
     config: {
-      provider: "cloudinary",
+      sizeLimit: 250 * 1024 * 1024, // 256mb in bytes
       providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
+        localServer: {
+          maxage: 300000,
+        },
       },
     },
   },
