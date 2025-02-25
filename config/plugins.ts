ffdbd0675d261870f70f3be3c8ed55f9b36c84f1
oeleomar <eleomar.dev@gmail.com>
2025-02-25 12:44:45 -0400

    add cloudnary

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,11 +1,16 @@
 export default ({ env }) => ({
   upload: {
     config: {
-      sizeLimit: 250 * 1024 * 1024, // 256mb in bytes
+      provider: "cloudinary",
       providerOptions: {
-        localServer: {
-          maxage: 300000,
-        },
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
